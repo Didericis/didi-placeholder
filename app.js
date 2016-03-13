61eb5ec25e8c31ba3b840ba4f4c022acca32bb2c
didericis <didericis@gmail.com>
2016-03-13 14:59:42 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+    stylus = require('stylus'),
+    path = require('path'),
+    morgan = require('morgan'),
+    nib = require('nib');
+
+var app = express();
+
+function compile(str, path) {
+    return stylus(str).set('filname', path).use(nib());
+}
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(morgan('combined'));
+app.use(stylus.middleware({
+    src: path.join(__dirname, 'public'),
+    compile: compile
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function(req, res) {
+    res.render('index', {
+        title : 'Home'
+    });
+});
+app.listen(3000);
